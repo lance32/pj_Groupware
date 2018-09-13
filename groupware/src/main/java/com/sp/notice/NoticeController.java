5f6dfd583e4223745e9fab5e273f2e91893117c5
dohyun2008 <user@user-pc>
2018-09-13 17:00:37 +0900

    내용없는 error 처리 페이지 / error처리 페이지 sidetabs JSP파일추가, 공지사항 데이터 추가 관련 내용 추가, 공지사항 mapper 추가

--- groupware/src/main/java/com/sp/notice/NoticeController.java
+++ groupware/src/main/java/com/sp/notice/NoticeController.java
@@ -1,12 +1,16 @@
 package com.sp.notice;
 
+import java.io.File;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.sp.common.FileManager;
-import com.sp.common.MyUtil;
+import com.sp.member.SessionInfo;
 
 @Controller("notice.NoticeController")
 public class NoticeController {
@@ -14,11 +18,11 @@ public class NoticeController {
 	@Autowired
 	private NoticeService service;
 	
-	@Autowired
-	private MyUtil util;
+//	@Autowired
+//	private MyUtil util;
 	
-	@Autowired
-	private FileManager fileManager;
+//	@Autowired
+//	private FileManager fileManager;
 	
 	@RequestMapping(value="/notice/list")
 	public String list() {
@@ -26,13 +30,31 @@ public class NoticeController {
 	}
 	
 	@RequestMapping(value="/notice/created", method=RequestMethod.GET)
-	public String createForm() {
+	public String createForm(
+			Model model,
+			HttpSession session
+			) {
+		SessionInfo info = (SessionInfo)session.getAttribute("member");
+		if(! info.getUserId().equals("admin")) {
+			return "redirect:/notice/list";
+		}
+		model.addAttribute("mode", "created");
 		return ".notice.created";
 	}
 	
 	@RequestMapping(value="/notice/created", method=RequestMethod.POST)
-	public String createSubmit() {
-		return "redircet:/notice/list";
+	public String createSubmit(
+			Notice dto,
+			HttpSession session) {
+		
+		SessionInfo info = (SessionInfo)session.getAttribute("member");
+		if(info.getUserId().equals("admin")) {
+			String root = session.getServletContext().getRealPath("/");
+			String pathname = root+"uploads"+File.separator+"notice";
+			dto.setMemberNum(info.getUserId());
+			service.insertNotice(dto, pathname);
+		}
+		return "redirect:/notice/list";
 	}
 	
 }
--- groupware/src/main/java/com/sp/notice/NoticeServiceImpl.java
+++ groupware/src/main/java/com/sp/notice/NoticeServiceImpl.java
@@ -3,15 +3,35 @@ package com.sp.notice;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sp.common.FileManager;
+import com.sp.common.dao.CommonDAO;
 
 @Service("notice.noticeService")
 public class NoticeServiceImpl implements NoticeService {
 
+	@Autowired
+	private CommonDAO dao;
+	
+	@Autowired
+	private FileManager fileManager;
+	
 	@Override
 	public int insertNotice(Notice dto, String pathname) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		try {
+			if(dto.getUpload() != null && ! dto.getUpload().isEmpty()) {
+				String saveFilename = fileManager.doFileUpload(dto.getUpload(), pathname);
+				dto.setSaveFilename(saveFilename);
+				dto.setOriginalFilename(dto.getUpload().getOriginalFilename());
+			}
+			result = dao.insertData("notice.insertNotice", dto);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return result;
 	}
 
 	@Override
