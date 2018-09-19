73ceae1e7d725dcab6318002901ef2f2648c5cee
dohyun2008 <user@user-pc>
2018-09-19 19:32:19 +0900

    동적 게시판 게시판 별 게시물 추가/리스트 출력 완성/게시물 조회부터 수정 필요함

--- groupware/src/main/java/com/sp/cboard/BoardController.java
+++ groupware/src/main/java/com/sp/cboard/BoardController.java
@@ -1,7 +1,12 @@
 package com.sp.cboard;
 
+import java.io.File;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +15,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sp.boardManage.BoardManage;
 import com.sp.boardManage.BoardManageService;
 import com.sp.common.FileManager;
 import com.sp.common.MyUtil;
+import com.sp.member.SessionInfo;
 
 @Controller("cboard.boardController")
 public class BoardController {
@@ -31,10 +38,67 @@ public class BoardController {
 	@RequestMapping(value="/cb_{board}/list")
 	public String list(
 			@PathVariable String board,
-			Model model
-			) {
+			@RequestParam(value="page", defaultValue="1") int current_page,
+			@RequestParam(value="searchKey", defaultValue="subject") String searchKey,
+			@RequestParam(value="searchValue", defaultValue="") String searchValue,
+			HttpServletRequest req,
+			Model model,
+			Map<String, Object> paramMap
+			) throws Exception {
+		String cp = req.getContextPath();
+		
+		BoardManage cb = mservice.readBoardManage("cb_"+board);
+		if(cb == null)
+			// 수정 필요
+			return "redirect:/";
+		
+		paramMap.put("tableName", cb.getTableName());
+		paramMap.put("searchKey", searchKey);
+		paramMap.put("searchValue", searchValue);
+		paramMap.put("canAnswer", cb.getCanAnswer());
+		
+		int rows = 10;
+		int dataCount = service.dataCount(paramMap);
+		int total_page = util.pageCount(rows, dataCount);
+		
+		if(current_page > total_page)
+			current_page = total_page;
+		
+		int start = (current_page-1) * rows + 1;
+		int end = (current_page) * rows;
+		
+		paramMap.put("start", start);
+		paramMap.put("end", end);
+		
+		// 게시판 목록(사이드 메뉴 출력용)
 		List<BoardManage> boardList = mservice.listBoardManage();
+		List<Board> list = service.listBoard(paramMap);
+		
+        String query = "";
+        String listUrl = ""; 
+        String articleUrl = "";
+        if(searchValue.length()!=0) {
+        	query = "searchKey=" +searchKey + "&searchValue=" + URLEncoder.encode(searchValue, "utf-8");	
+        }
+        
+        if(query.equals("")) {
+        	listUrl = cp+"/cb_"+board+"/list";
+        	articleUrl = cp+"/cb_"+board+"/article?page=" + current_page;
+        } else {
+        	listUrl = cp+"/cb_"+board+"/list?" + query;
+        	articleUrl = cp+"/cb_"+board+"/article?page=" + current_page + "&"+ query;
+        }
+		String paging = util.paging(current_page, total_page, listUrl);
+        
+		model.addAttribute("cb", cb);
 		model.addAttribute("boardList", boardList);
+		model.addAttribute("list", list);
+		model.addAttribute("articleUrl", articleUrl);
+		model.addAttribute("page", current_page);
+		model.addAttribute("total_page", total_page);
+		model.addAttribute("dataCount", dataCount);
+		model.addAttribute("boardList", boardList);
+		model.addAttribute("paging", paging);		
 		
 		return ".cboard.list";
 	}
@@ -45,9 +109,118 @@ public class BoardController {
 	          HttpSession session,
 	          Model model
 			) {
-		// 글쓰기 권한 처리 필요
+		BoardManage cb = mservice.readBoardManage("cb_"+board);
+   	    if(cb==null) {
+   	    	return "redirect:/";
+   	    }
+   	    
+   	    // 글쓰기 권한 처리 필요(session 활용)
+   	    /*
+		 SessionInfo info=(SessionInfo)session.getAttribute("member");
+			if(cb.getMemberLevel()>info.getMemberLevel()) {
+			return ".member.noAuthorized";
+			}
+			형태로 구현
+   	     * */
+   	    
+		// 게시판 목록(사이드 메뉴 출력용)
+		List<BoardManage> boardList = mservice.listBoardManage();
 		
+		model.addAttribute("boardList", boardList);
 		model.addAttribute("mode", "created");
+		model.addAttribute("cb", cb);
+		
 		return ".cboard.created";
+	}
+	
+	@RequestMapping(value="/cb_{board}/created", method=RequestMethod.POST)
+	public String createdSubmit(
+			@PathVariable String board,
+			Board dto,
+			HttpSession session,
+			Model model
+			) {
+		// 글쓰기 권한 처리 필요
+		SessionInfo info = (SessionInfo)session.getAttribute("member");
+		
+		BoardManage cb=mservice.readBoardManage("cb_"+board);
+   	    if(cb==null) {
+   	    	return "redirect:/";
+   	    }
+   	    
+   	    String root = session.getServletContext().getRealPath("/");
+   	    String pathname = root + "uploads" + File.separator + "cboard";
+   	    
+   	    dto.setTableName(cb.getTableName());
+   	    dto.setMemberNum(info.getUserId());
+   	    
+   	    service.insertBoard(dto, "created", pathname);
+   	    
+   	    return "redirect:/cb_"+board+"/list";
+	}
+	
+	@RequestMapping(value="/cb_{board}/article", method=RequestMethod.GET)
+	public String article(
+			@PathVariable String board,
+			@RequestParam(value="num") int num,
+			@RequestParam(value="page") String page,
+			@RequestParam(value="searchKey", defaultValue="subject") String searchKey,
+			@RequestParam(value="searchValue", defaultValue="") String searchValue,
+			Map<String, Object> paramMap,
+			HttpSession session,
+			Model model
+			) throws Exception {
+		
+		BoardManage cb=mservice.readBoardManage("cb_"+board);
+   	    if(cb==null) {
+   	    	return "redirect:/";
+   	    }
+   	    searchValue = URLDecoder.decode(searchValue, "UTF-8");
+		String query = "page="+page;
+		if(searchValue.length() != 0) {
+			query += "&searchKey="+searchKey+"&searchValue="+URLEncoder.encode(searchValue, "UTF-8");
+		}
+		
+		paramMap.put("tableName", cb.getTableName());
+		paramMap.put("canAnswer", cb.getCanAnswer());
+		paramMap.put("num", num);
+		
+		service.updateHitCount(paramMap);
+		
+		Board dto = service.readBoard(paramMap);
+		if(dto == null)
+			return "redirect:/cb_"+board+"/list?"+query;
+		dto.setContent(util.htmlSymbols(dto.getContent()));
+		
+		paramMap.put("searchKey", searchKey);
+		paramMap.put("searchValue", searchValue);
+		if(cb.getCanAnswer()==1) {
+			paramMap.put("groupNum", dto.getGroupNum());
+			paramMap.put("orderNo", dto.getOrderNo());
+		}
+		
+		Board preReadDto = service.preReadBoard(paramMap);
+		Board nextReadDto = service.nextReadBoard(paramMap);
+		
+		int boardLikeCount = 0;
+		if(cb.getCanLike() == 1) {
+			boardLikeCount = service.boardLikeCount(paramMap);
+		}
+		List<Board> listFile = service.listFile(paramMap);
+		
+		// 게시판 목록(사이드 메뉴 출력용)
+		List<BoardManage> boardList = mservice.listBoardManage();
+		
+		model.addAttribute("dto", dto);
+		model.addAttribute("preReadDto", preReadDto);
+		model.addAttribute("nextReadDto", nextReadDto);
+		model.addAttribute("boardLikeCount", boardLikeCount);
+		model.addAttribute("listFile", listFile);
+		model.addAttribute("page", page);
+		model.addAttribute("query", query);
+		model.addAttribute("boardList", boardList);
+		model.addAttribute("cb", cb);
+		
+		return ".cboard.article";
 	}
 }
--- groupware/src/main/webapp/WEB-INF/view/cboard/created.jsp
+++ groupware/src/main/webapp/WEB-INF/view/cboard/created.jsp
@@ -7,10 +7,136 @@
 %>
 
 <script>
-function searchList() {
-	var f=document.searchForm;
-	f.submit();
-}
-</script>
+$(function(){
+	$("#btnSend").click(function(){
+		var subVal = $('input[name=title]').val();
+		var contentVal = $('textarea').val();
+		
+		if(!subVal || !contentVal){
+			alert("입력하지 않은 영역이 있습니다.");
+			return;
+		}
+        $("form[name=boardForm]").attr("action","<%=cp%>/${cb.tableName}/${mode}");
+        $("form[name=boardForm]").submit();
+	});
+	
+	$(".btnReset").click(function(){
+		$(".moreFile").remove();
+	});
+});
 
-게 시 물 작 성 화 면
+
+<c:if test="${mode=='update' && cb.canFile=='1'}">
+function deleteFile(fileNum) {
+		var url="<%=cp%>/${cb.tableName}/deleteFile";
+		$.post(url, {fileNum:fileNum}, function(data){
+			$("#f"+fileNum).remove();
+		}, "json");
+}
+</c:if>
+
+<c:if test="${cb.canFile=='1'}">
+$(function(){
+  	$("body").on("change", "input[name=upload]", function(){
+  		if(! $(this).val()) {
+  			return;	
+  		}
+  		
+  		var b=false;
+  		$("input[name=upload]").each(function(){
+  			if(! $(this).val()) {
+  				b=true;
+  				return;
+  			}
+  		});
+  		if(b)
+  			return;
+
+  		var $tr, $td, $input;
+  		
+  	    $tr=$("<tr height='40' class='moreFile'>");
+  	    $td=$("<td>", {width:"100", style:"text-align: left;", html:""});
+  	    $tr.append($td);
+  	    $td=$("<td>");
+  	    $input=$("<input>", {type:"file", name:"upload", class:"boxTF", style:"width: 98%; height: 25px;"});
+  	    $td.append($input);
+  	    $tr.append($td);
+  	    
+  	    $("#tb").append($tr);
+  	});
+});
+</c:if>
+
+</script>
+<div style="clear: both; margin: 10px 0px 15px 10px;">
+	<span class="glyphicon glyphicon-bullhorn"
+		style="font-size: 28px; margin-left: 10px;"></span> <span
+		style="font-size: 30px;">&nbsp;${cb.boardName }</span><br>
+	<div style="clear: both; width: 300px; height: 1px; border-bottom: 3px solid black;"></div>
+</div>
+
+<div class="container">
+	<form name="boardForm" method="post" enctype="multipart/form-data">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="name">작성자</label>
+					<input type="text" class="form-control" name="name" readonly="readonly" value="${sessionScope.member.userName}">
+				</div>
+			</div>
+
+		</div>
+
+		<div class="form-group">
+			<label for="subject">제 목</label>
+			<input type="text" class="form-control" name="title" placeholder="제목을 입력하세요." value="${dto.title}">
+		</div>
+
+
+		<div class="form-group">
+			<label for="content">내 용</label>
+			<textarea class="form-control" rows="20" name="content" style="resize: none;">${dto.content }</textarea>
+		</div>
+		
+		<c:if test="${cb.canFile == '1'}">
+			<div class="form-group" id="tb">
+				<label for="File">파 일 첨 부</label> <input type="file" name="upload"><br>
+			</div>
+		</c:if>
+		
+		<c:if test="${mode=='update'}">
+			<c:forEach var="vo" items="${listFile}">
+				  <tr align="left" height="40" style="border-bottom: 1px solid #cccccc;" id="f${vo.fileNum}">
+				      <td width="100" bgcolor="#eeeeee" style="text-align: center;">첨부된파일</td>
+				      <td style="padding-left:10px;"> 
+				          ${dto.originalFilename}
+				          <c:if test="${not empty dto.saveFilename}">
+				          		| <a id="chkDelete">파일삭제</a>
+				          </c:if>
+				       </td>
+				  </tr>
+			</c:forEach>
+		</c:if>
+
+		<div class="center-block" style='width: 300px'>
+			<button type="button" class="btn" id="btnSend">${mode=='update'?'수 정 완 료':'등 록 하 기'}</button>
+			<button type="reset" class="btn btnReset">다 시 쓰 기</button>
+			<button type="button" class="btn" onclick="javascript:location.href='<%=cp%>/${cb.tableName}/list';">${mode=='update'?'수 정 취 소':'뒤 로 가 기'}</button>
+		</div>
+		
+		<c:if test="${mode=='update'}">
+			<input type="hidden" name="num" value="${dto.num}">		
+			<input type="hidden" name="saveFilename" value="${dto.saveFilename}">
+			<input type="hidden" name="originalFilename" value="${dto.originalFilename}">
+			<input type="hidden" name="page" value="${page}">
+		</c:if>
+		
+		<c:if test="${mode=='answer'}">
+			<input type="hidden" name="page" value="${page}">
+			<input type="hidden" name="groupNum" value="${dto.groupNum}">
+			<input type="hidden" name="orderNo" value="${dto.orderNo}">
+			<input type="hidden" name="depth" value="${dto.depth}">
+			<input type="hidden" name="parent" value="${dto.num}">
+		</c:if>
+	</form>
+</div>
