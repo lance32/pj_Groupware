09eba0f68f035c292bd809c7c570c587c944b101
dohyun2008 <dohyun2008@naver.com>
2018-09-28 17:51:34 +0900

    자원관리 화면 제작 중 / 스케쥴러 js,css 파일 추가 / event 부분 처리 중

---
+++ groupware/src/main/java/com/sp/resource/Resource.java
@@ -0,0 +1,217 @@
+package com.sp.resource;
+
+public class Resource {
+	private int groupNum;
+	private String groupName;
+	
+	private int resourceNum;
+	private String resourceName;
+	private String color;
+	private int occupancy;
+	
+	private int reserveNum;
+	private String memberNum;
+	private String name;
+	private String startDay;
+	private String endDay;
+	private String startTime;
+	private String endTime;
+	private int inwon;
+	private String created;
+	private String title;
+	private String content;
+	
+	private String allDay;
+	
+	private int alarm;
+	private int alarmTime;
+	private String alarmTitle;
+	private String alarmContent;
+	
+	private int alarmNum;
+	
+	// getter, setter
+	public int getGroupNum() {
+		return groupNum;
+	}
+
+	public void setGroupNum(int groupNum) {
+		this.groupNum = groupNum;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public int getResourceNum() {
+		return resourceNum;
+	}
+
+	public void setResourceNum(int resourceNum) {
+		this.resourceNum = resourceNum;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getOccupancy() {
+		return occupancy;
+	}
+
+	public void setOccupancy(int occupancy) {
+		this.occupancy = occupancy;
+	}
+
+	public int getReserveNum() {
+		return reserveNum;
+	}
+
+	public void setReserveNum(int reserveNum) {
+		this.reserveNum = reserveNum;
+	}
+
+	public String getMemberNum() {
+		return memberNum;
+	}
+
+	public void setMemberNum(String memberNum) {
+		this.memberNum = memberNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStartDay() {
+		return startDay;
+	}
+
+	public void setStartDay(String startDay) {
+		this.startDay = startDay;
+	}
+
+	public String getEndDay() {
+		return endDay;
+	}
+
+	public void setEndDay(String endDay) {
+		this.endDay = endDay;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public int getInwon() {
+		return inwon;
+	}
+
+	public void setInwon(int inwon) {
+		this.inwon = inwon;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAllDay() {
+		return allDay;
+	}
+
+	public void setAllDay(String allDay) {
+		this.allDay = allDay;
+	}
+
+	public int getAlarm() {
+		return alarm;
+	}
+
+	public void setAlarm(int alarm) {
+		this.alarm = alarm;
+	}
+
+	public int getAlarmTime() {
+		return alarmTime;
+	}
+
+	public void setAlarmTime(int alarmTime) {
+		this.alarmTime = alarmTime;
+	}
+
+	public String getAlarmTitle() {
+		return alarmTitle;
+	}
+
+	public void setAlarmTitle(String alarmTitle) {
+		this.alarmTitle = alarmTitle;
+	}
+
+	public String getAlarmContent() {
+		return alarmContent;
+	}
+
+	public void setAlarmContent(String alarmContent) {
+		this.alarmContent = alarmContent;
+	}
+
+	public int getAlarmNum() {
+		return alarmNum;
+	}
+
+	public void setAlarmNum(int alarmNum) {
+		this.alarmNum = alarmNum;
+	}
+}
